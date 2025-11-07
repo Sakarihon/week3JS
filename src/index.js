acd3755d26fda0623716da627b73b0b3e561c254
sakari Honkavaara <Sakari.honkavaara@metropolia.fi>
2025-11-07 10:32:19 +0200

    s

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import app from './api/app.js';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
